e3b296c0f90d6a511eef8662d800d3a3e7e1acbe
sushantraj18 <codexsushant@gmail.com>
2025-02-07 23:49:58 +0530

    auth jwt admin page route protect logic

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const authMiddleware = require("../middleware/auth-middleware")
+const adminMiddleware = require("../middleware/admin-middleware")
 
 
 const router = express.Router()
 
-router.get("/welcome",authMiddleware,(req,res)=>{
+router.get("/welcome",authMiddleware,adminMiddleware,(req,res)=>{
     res.json({message : "welcome to admin page"})
 })
 
